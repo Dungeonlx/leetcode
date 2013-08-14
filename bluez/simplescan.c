afc756083d2092e52a72fe8b5582ebefd3f48b3f
xiangzhai <xiangzhai83@gmail.com>
2013-08-14 22:44:05 +0800

    Remove useless file

--- bluez/simplescan.c
+++ bluez/simplescan.c
@@ -6,17 +6,17 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     inquiry_info *ii = NULL;
     int max_rsp, num_rsp;
     int dev_id, sock, len, flags;
     int i;
-    char addr[19] = { 0 };
-    char name[248] = { 0 };
+    char addr[19] = {0};
+    char name[248] = {0};
 
     dev_id = hci_get_route(NULL);
-    sock = hci_open_dev( dev_id );
+    sock = hci_open_dev(dev_id);
     if (dev_id < 0 || sock < 0) {
         perror("opening socket");
         exit(1);
@@ -28,7 +28,8 @@ int main(int argc, char **argv)
     ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
     
     num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
-    if( num_rsp < 0 ) perror("hci_inquiry");
+    if (num_rsp < 0) 
+        perror("hci_inquiry");
 
     for (i = 0; i < num_rsp; i++) {
         ba2str(&(ii+i)->bdaddr, addr);
@@ -39,7 +40,10 @@ int main(int argc, char **argv)
         printf("%s  %s\n", addr, name);
     }
 
-    free( ii );
-    close( sock );
+    free(ii);
+    ii = NULL;
+    close(sock);
+    sock = -1;
+
     return 0;
 }
