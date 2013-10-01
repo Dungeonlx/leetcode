72c52758254e52e7c8851663ab49fb1eb16a484f
xiangzhai <xiangzhai83@gmail.com>
2013-10-01 21:11:58 +0800

    Update dict txt

--- palindrome/find_word.cpp
+++ palindrome/find_word.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 #include <locale>
 #include <algorithm>
 #include <time.h>
 
 namespace String 
 {
-    // string tolower
+    bool IsTheChar(std::string str, int c) 
+    {
+        str.erase(std::remove(str.begin(), str.end(), c), str.end());
+
+        if (str.size() == 0)
+            return true;
+
+        return false;
+    }
+
     std::string ToLower(std::string str)                                      
     {                                                                           
         std::string ret = "";                                                   
@@ -28,16 +38,6 @@ namespace String
     }
 };
 
-namespace Time 
-{
-    void ToElapse(unsigned elapsed) 
-    {
-        int sec = elapsed / CLOCKS_PER_SEC;
-
-        std::cout << "elapsed: " << sec << "." << elapsed - sec * CLOCKS_PER_SEC << std::endl;
-    }
-};
-
 class Dict 
 {
 public:
@@ -48,6 +48,9 @@ public:
 
         while (std::getline(DictFile, strLine)) 
         {
+            if (String::IsTheChar(strLine, ' ')) 
+                continue;
+            
             if (isToLower) 
                 m_WordList.push_back(String::ToLower(strLine));
             else
@@ -62,12 +65,11 @@ public:
 
         std::cout << m_WordList.size() << " words" << std::endl;
     }
-    virtual ~Dict() 
-    {
-    
-    }
+    virtual ~Dict() {}
 
 public:
+    std::vector<std::string> getWordList() { return m_WordList; }
+
     // 4 DEBUG
     void printWordList() 
     {
@@ -79,6 +81,58 @@ private:
     std::vector<std::string> m_WordList;
 };
 
+bool isInRange(std::map<int, int> m, int p, int s) 
+{
+    std::map<int, int>::iterator iter;
+
+    for (iter = m.begin(); iter != m.end(); iter++) 
+    {
+        if (iter->first <= p) 
+        {
+            if (p + s <= iter->first + iter->second) 
+                return true;
+        }
+
+        if (p <= iter->first) 
+        {
+            if (iter->first <= p + s) 
+                return true;
+        }
+    }
+
+    return false;
+}
+
+void solution(std::string str, std::vector<std::string> WordList) 
+{
+    std::map<int, int> PosLengthMap;
+    std::map<int, int>::iterator PosLengthIter;
+    std::string NewStr = "";
+    int FindPos;
+    int i;
+
+    for (i = 0; i < WordList.size(); i++) 
+    {
+        FindPos = String::ToLower(str).find(WordList[i]);
+        
+        if (FindPos != std::string::npos) 
+        {
+            if (isInRange(PosLengthMap, FindPos, WordList[i].size())) 
+                continue;
+            PosLengthMap[FindPos] = WordList[i].size();
+        }
+    }
+
+    for (PosLengthIter = PosLengthMap.begin(); 
+         PosLengthIter != PosLengthMap.end(); PosLengthIter++) 
+    {
+        //std::cout << PosLengthIter->first << " " << PosLengthIter->second << std::endl;
+        NewStr += str.substr(PosLengthIter->first, PosLengthIter->second) + " ";
+    }
+
+    std::cout << NewStr << std::endl;
+}
+
 int main(int argc, char* argv[]) 
 {
     unsigned t0 = clock();
@@ -86,13 +140,18 @@ int main(int argc, char* argv[])
     //=========================================================================
     
     Dict objDict("dict.txt");
-
     //objDict.printWordList();
+    
+    std::ifstream file("gettysburg.txt");
+    std::string str;
+    file >> str;
+    std::cout << str << std::endl;
+    solution(str, objDict.getWordList());
 
     // END
     //=========================================================================
     unsigned elapsed = clock() - t0;
-    Time::ToElapse(elapsed);
+    std::cout << "elapsed " << elapsed << std::endl;
 
     return 0;
 }
